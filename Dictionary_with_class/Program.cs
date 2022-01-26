5d1f5799968c5988f09029d5794472fec961b0dd
NikDorosh <doroshinsky183@gmail.com>
2022-01-26 09:30:16 +0300

    Add collections and generics

---
+++ Dictionary_with_class/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_with_class
+{
+    class Program
+    {
+       public static Dictionary<string, string> countries = new Dictionary<string, string>
+        {
+            ["Франция"] = "Париж",
+            ["Германия"] = "Берлин",
+            ["Великобритания"] = "Лондон"
+        };
+        static void Main(string[] args)
+        {
+            Dictionary<char, Person> people = new Dictionary<char, Person>();
+            people.Add('b', new Person() { Name = "Bill" });
+            people.Add('t', new Person() { Name = "Tom" });
+            people.Add('j', new Person() { Name = "John" });
+
+            foreach (KeyValuePair<char, Person> keyValue in people)
+            {
+                // keyValue.Value представляет класс Person
+                Console.WriteLine(keyValue.Key + " - " + keyValue.Value.Name);
+            }
+
+            // перебор ключей
+            foreach (char c in people.Keys)
+            {
+                Console.WriteLine(c);
+            }
+
+            // перебор по значениям
+            foreach (Person p in people.Values)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+            foreach(KeyValuePair<string,string> keyvalue in countries)
+            {
+                Console.WriteLine(keyvalue.Key + " - " + keyvalue.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ObservableCollection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<User> users = new ObservableCollection<User>
+            {
+                new User { Name = "Bill"},
+                new User { Name = "Tom"},
+                new User { Name = "Alice"}
+            };
+
+            users.CollectionChanged += Users_CollectionChanged;
+
+            users.Add(new User { Name = "Bob" });
+            users.RemoveAt(1);
+            users[0] = new User { Name = "Anders" };
+
+            foreach (User user in users)
+            {
+                Console.WriteLine(user.Name);
+            }
+
+            Console.Read();
+        }
+
+        private static void Users_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case NotifyCollectionChangedAction.Add: // если добавление
+                    User newUser = e.NewItems[0] as User;
+                    Console.WriteLine($"Добавлен новый объект: {newUser.Name}");
+                    break;
+                case NotifyCollectionChangedAction.Remove: // если удаление
+                    User oldUser = e.OldItems[0] as User;
+                    Console.WriteLine($"Удален объект: {oldUser.Name}");
+                    break;
+                case NotifyCollectionChangedAction.Replace: // если замена
+                    User replacedUser = e.OldItems[0] as User;
+                    User replacingUser = e.NewItems[0] as User;
+                    Console.WriteLine($"Объект {replacedUser.Name} заменен объектом {replacingUser.Name}");
+                    break;
+            }
+        }
+    }
+
+    class User
+    {
+        public string Name { get; set; }
+    }
+}
