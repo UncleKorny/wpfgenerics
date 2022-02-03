c413e108e0ff20b4ffb303313087ec0a7a080d2f
Korny <themkornygame@gmail.com>
2022-02-03 17:16:29 +0300

    fix

--- Generics_wpf/Generics_wpf/MainWindow.xaml.cs
+++ Generics_wpf/Generics_wpf/MainWindow.xaml.cs
@@ -26,15 +26,15 @@ namespace Generics_wpf
         {
             InitializeComponent();
             dataGridView1.ItemsSource = result;
+            dataGridView2.ItemsSource = result2;
         }
         ObservableCollection<DataItem> result = new ObservableCollection<DataItem>();
-
+        ObservableCollection<DataItem> result2 = new ObservableCollection<DataItem>();
         private void button_Click(object sender, RoutedEventArgs e)
         {
             float x = (float)XnumericUpDown.Value;
             float y = (float)YnumericUpDown.Value;
             result.Add(new DataItem(x,y));
-            
         }
 
         private void mainform_Loaded(object sender, RoutedEventArgs e)
@@ -53,11 +53,51 @@ namespace Generics_wpf
                 var num = dataGridView1.SelectedIndex;
                 result.RemoveAt(num);
                 dataGridView1.ItemsSource = result;
+                
             }
             catch
             {
                 MessageBox.Show("Выберите точку!");
             }
+        }
+
+        private void solvebtn_Click(object sender, RoutedEventArgs e)
+        {
+            result2.Clear();
+            dataGridView2.ItemsSource = "";
+            foreach(DataItem dt in result)
+            {
+                if (dt.x < 0) result2.Add(dt);
+            }
+            foreach(DataItem dt in result2)
+            {
+                result.Remove(dt);
+            }
+            dataGridView2.ItemsSource = result2;
+        }
+
+        private void closebtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void sortbtn_Click(object sender, RoutedEventArgs e)
+        {
+            int temp;
+            foreach(DataItem dt in result)
+            {
+                for(int j = 1; j < result.Count; j++)
+                {
+                    
+                }
+            }
+            //for(int i = 0; i < result2.Count; i++)
+            //{
+            //    for(int j = i + 1; j < result2.Count; j++)
+            //    {
+            //        if()
+            //    }
+            //}
         }
     }
 }
